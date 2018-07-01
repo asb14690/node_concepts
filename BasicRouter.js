5a7238802b1f54164eb834a09bd1ee7afba1ded8
Ankur <ankur.sharma@technocube.in>
2018-07-01 09:53:22 +0530

    added basic routing

---
+++ BasicRouter.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const http = require('http');
+const url = require('url');
+
+let routes = {
+    'GET':{
+        '/': (req,res) => {
+            res.writeHead(200,{'content-type':'text/html'});
+            res.write('<h1>Hello Routes</h1>')
+        }
+    },
+    'POST':{
+
+    },
+    'NA':(req,res) => {
+        res.writeHead(404,{'content-type':'text/html'});
+        res.write('<h1 style="text-align: center">404 Bad Gateway</h1>\n<p style="text-align: center">Node.js server 8.11.3</p>');
+    }
+}
+
+function router(req,res){
+    let baseURI = url.parse(req.url, true);
+    let resolveRoute = routes[req.method][baseURI.pathname];
+    if(resolveRoute != undefined){
+        resolveRoute(req,res);
+    }else {
+
+        routes['NA'](req,res);
+    }
+};
+
+http
+    .createServer(router)
+    .listen(3000, () => {
+        console.log('Server is up and running on PORT 3000');
+    });
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # node_concepts
 Node.js comoponents
+
+## Basic Introduction of Node.js
+Nodejs is a platform building scalable, high performance networked web application using JavaScript.
+
