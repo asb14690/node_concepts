c34ea60bb21e2c0cfed3c3924f9fbf99eb2b87fe
Ankur <ankur.sharma@technocube.in>
2018-07-01 08:44:51 +0530

    added the basic file for node.js server

---
+++ server.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const http = require('http');
+
+http
+    .createServer((req,res) => {
+        res.writeHead(200,{'content-type':'text/html'});
+        res.write('<h1>Welcome to Node.js Server</h1>')
+    })
+    .listen(3000, () => {
+        console.log('Server is up and running at 3000');
+    });
